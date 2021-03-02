13b35974f5811c608b68accf4600727f486929ee
hymmns <social.hymmns@gmail.com>
2021-03-02 19:17:35 +0100

    updated the execel data

--- sw.js
+++ sw.js
@@ -1,27 +1,24 @@
 const cachedRostell = 'rostell-cache';
 const assetsToChache = [
-  '/rostell/',
-  '/rostell/app.html',
-  '/rostell/app.js',
-  'rostell/icon/favicon-32x32.png',
-  '/rostell/manifest.webmanifest',
-  '/rostell/icon/icon.png',
-  '/rostell/style.css',
+  './rostell/',
+  './rostell/index.html',
+  './rostell/app.js',
+  './rostell/icon/favicon-32x32.png',
+  './rostell/manifest.webmanifest',
+  './rostell/icon/icon.png',
+  './rostell/style.css',
 ];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(cachedRostell).then((cache) => {
       cache.addAll(assetsToChache);
+						console.log('assets successfully cached');
     })
   );
 });
 
 self.addEventListener('fetch', (e) => {
   console.log(e.request.url);
-  e.respondWith(
-    caches.match(e.request).then((res) => {
-      return res || fetch(e.request);
-    })
-  );
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
